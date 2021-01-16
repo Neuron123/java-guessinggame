861f63fc4ead1ebbaa86e7d9add435c38c31b113
DarthV <DarthV@yahoo.com>
2021-01-16 14:55:56 +0300

    This is a simple program that lets user guess a number

---
+++ src/main/java/numberguess/Main.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numberguess;
+import javax.swing.*;
+
+/**
+ *
+ * @author bryko
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int computerNumber = (int) (Math.random()*100 + 1); 
+        int userAnswer = 0;
+        System.out.println("The correct guess would be " + computerNumber);
+        int count = 1;
+        
+            while (userAnswer != computerNumber)
+        {
+            String response = JOptionPane.showInputDialog(null,
+                "Enter a guess between 1 and 100", "Guessing Game", 3);
+            userAnswer = Integer.parseInt(response);
+            JOptionPane.showMessageDialog(null, ""+ determineGuess(userAnswer, computerNumber, count));
+            count++;
+        }  
+    }
+    
+    public static String determineGuess(int userAnswer, int computerNumber, int count){
+        if (userAnswer <=0 || userAnswer >100) {
+            return "Your guess is invalid";
+        }
+        else if (userAnswer == computerNumber ){
+            return "Correct!\nTotal Guesses: " + count;
+        }
+        else if (userAnswer > computerNumber) {
+            return "Your guess is too high, try again.\nTry Number: " + count;
+        }
+        else if (userAnswer < computerNumber) {
+            return "Your guess is too low, try again.\nTry Number: " + count;
+        }
+        else {
+            return "Your guess is incorrect\nTry Number: " + count;
+        }
+    }
+    
+}
